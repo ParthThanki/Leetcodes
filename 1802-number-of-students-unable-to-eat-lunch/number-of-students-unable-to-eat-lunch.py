124a4c4be9f601de4df74063b988b06099d115fc
Parth Thanki <88216725+ParthThanki@users.noreply.github.com>
2025-03-25 16:52:23 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (63.26%) - LeetSync

---
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        n = 0
+    
+        while students:
+            if students[0] == sandwiches[0]:
+                students.pop(0)
+                sandwiches.pop(0)
+            else:
+                students.append(students.pop(0))
+                if sandwiches[0] not in students:
+                    return len(students)
+            if not sandwiches:
+                return len(students)
