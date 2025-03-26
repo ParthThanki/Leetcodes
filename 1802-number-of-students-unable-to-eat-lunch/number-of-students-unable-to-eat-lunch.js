f45de88964caa7eb1931464b01d066185d1dc116
Parth Thanki <88216725+ParthThanki@users.noreply.github.com>
2025-03-25 17:15:59 -0700

    Time: 0 ms (100.00%) | Memory: 53.5 MB (46.80%) - LeetSync

---
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} students
+ * @param {number[]} sandwiches
+ * @return {number}
+ */
+var countStudents = function(students, sandwiches) {
+
+    let count = 0;
+
+    while(count < students.length && !!students.length) {
+
+        if(students[0] === sandwiches[0]) {
+            count = 0;
+            students.shift();
+            sandwiches.shift();
+        } else {
+            count++;
+            let val = students.shift();
+            students.push(val);
+        }
+    }
+
+    return count;
+};
